7527ca3628166f229b9c545868510dc57362fddc
wz2cool <wz2cool@live.cn>
2017-08-11 16:19:32 +0800

    add rowbounds by dynamic query

--- src/main/java/com/github/wz2cool/dynamic/mybatis/mapper/SelectRowBoundsByDynamicQueryMapper.java
+++ src/main/java/com/github/wz2cool/dynamic/mybatis/mapper/SelectRowBoundsByDynamicQueryMapper.java
@@ -5,11 +5,14 @@ import com.github.wz2cool.dynamic.mybatis.mapper.constant.MapperConstants;
 import com.github.wz2cool.dynamic.mybatis.mapper.provider.DynamicQueryProvider;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.session.RowBounds;
 
 import java.util.List;
 
 public interface SelectRowBoundsByDynamicQueryMapper<T> {
 
     @SelectProvider(type = DynamicQueryProvider.class, method = "dynamicSQL")
-    List<T> selectRowBoundsByDynamicQuery(@Param(MapperConstants.DYNAMIC_QUERY) DynamicQuery<T> dynamicQuery);
+    List<T> selectRowBoundsByDynamicQuery(
+            @Param(MapperConstants.DYNAMIC_QUERY) DynamicQuery<T> dynamicQuery,
+            RowBounds rowBounds);
 }
