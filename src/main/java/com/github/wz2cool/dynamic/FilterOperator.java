7a0203b457990d6f38cb666a4a1caa209766f4e8
wz2cool <wz2cool@live.cn>
2017-08-14 18:02:44 +0800

    test serializable

--- src/main/java/com/github/wz2cool/dynamic/FilterOperator.java
+++ src/main/java/com/github/wz2cool/dynamic/FilterOperator.java
@@ -10,73 +10,49 @@ public enum FilterOperator implements Serializable {
     /**
      * Less than filter operator.
      */
-    LESS_THAN("LESS_THAN", 1),
+    LESS_THAN,
     /**
      * Less than or equal filter operator.
      */
-    LESS_THAN_OR_EQUAL("LESS_THAN_OR_EQUAL", 2),
+    LESS_THAN_OR_EQUAL,
     /**
      * Equal filter operator.
      */
-    EQUAL("EQUAL", 3),
+    EQUAL,
     /**
      * Not equal filter operator.
      */
-    NOT_EQUAL("NOT_EQUAL", 4),
+    NOT_EQUAL,
     /**
      * Greater than or equal filter operator.
      */
-    GREATER_THAN_OR_EQUAL("GREATER_THAN_OR_EQUAL", 5),
+    GREATER_THAN_OR_EQUAL,
     /**
      * Greater than filter operator.
      */
-    GREATER_THAN("GREATER_THAN", 6),
+    GREATER_THAN,
     /**
      * Start with filter operator.
      */
-    START_WITH("START_WITH", 7),
+    START_WITH,
     /**
      * End with filter operator.
      */
-    END_WITH("END_WITH", 8),
+    END_WITH,
     /**
      * Contains filter operator.
      */
-    CONTAINS("CONTAINS", 9),
+    CONTAINS,
     /**
      * In filter operator.
      */
-    IN("IN", 10),
+    IN,
     /**
      * Not in filter operator.
      */
-    NOT_IN("NOT_IN", 11),
+    NOT_IN,
     /**
      * Between filter operator.
      */
-    BETWEEN("BETWEEN", 12);
-
-    private String name;
-    private int value;
-
-    FilterOperator(String name, int value) {
-        this.name = name;
-        this.value = value;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
+    BETWEEN
 }
