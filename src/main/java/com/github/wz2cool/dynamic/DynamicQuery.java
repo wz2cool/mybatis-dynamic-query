99fd1c8105c3a5456f1ed66ead5eebb1890afdf5
wz2cool <wz2cool@live.cn>
2018-11-14 15:30:15 +0800

    update version to 2.0.3-snapshot

--- src/main/java/com/github/wz2cool/dynamic/DynamicQuery.java
+++ src/main/java/com/github/wz2cool/dynamic/DynamicQuery.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 @SuppressWarnings("squid:S1948")
 public class DynamicQuery<T> implements Serializable {
@@ -20,6 +21,10 @@ public class DynamicQuery<T> implements Serializable {
 
     public DynamicQuery(Class<T> entityClass) {
         this.entityClass = entityClass;
+    }
+
+    public static <T> DynamicQuery<T> createQuery(Class<T> entityClass) {
+        return new DynamicQuery<>(entityClass);
     }
 
     public boolean isDistinct() {
@@ -54,48 +59,71 @@ public class DynamicQuery<T> implements Serializable {
         this.sorts = sorts;
     }
 
-    public boolean addFilter(FilterDescriptorBase newFilter) {
+    public DynamicQuery<T> addFilter(FilterDescriptorBase newFilter) {
         return addFilters(newFilter);
     }
 
     @SuppressWarnings("Duplicates")
-    public boolean addFilters(FilterDescriptorBase... newFilters) {
+    public DynamicQuery<T> addFilters(FilterDescriptorBase... newFilters) {
         List<FilterDescriptorBase> filtersCopy =
                 filters == null ? new ArrayList<>() : new ArrayList<>(Arrays.asList(filters));
         List<FilterDescriptorBase> newFilterList = Arrays.asList(newFilters);
         boolean result = filtersCopy.addAll(newFilterList);
-        this.setFilters(filtersCopy.toArray(new FilterDescriptorBase[filtersCopy.size()]));
-        return result;
+        if (result) {
+            this.setFilters(filtersCopy.toArray(new FilterDescriptorBase[filtersCopy.size()]));
+        }
+        return this;
     }
 
     @SuppressWarnings("Duplicates")
-    public boolean removeFilter(FilterDescriptorBase removeFilter) {
+    public DynamicQuery<T> removeFilter(FilterDescriptorBase removeFilter) {
         List<FilterDescriptorBase> filtersCopy =
                 filters == null ? new ArrayList<>() : new ArrayList<>(Arrays.asList(filters));
         boolean result = filtersCopy.remove(removeFilter);
-        this.setFilters(filtersCopy.toArray(new FilterDescriptorBase[filtersCopy.size()]));
-        return result;
+        if (result) {
+            this.setFilters(filtersCopy.toArray(new FilterDescriptorBase[filtersCopy.size()]));
+        }
+        return this;
     }
 
 
-    public boolean addSorts(SortDescriptorBase... newFilters) {
+    public DynamicQuery<T> addSorts(SortDescriptorBase... newFilters) {
         List<SortDescriptorBase> sortsCopy =
                 sorts == null ? new ArrayList<>() : new ArrayList<>(Arrays.asList(sorts));
         List<SortDescriptorBase> newSortLists = Arrays.asList(newFilters);
         boolean result = sortsCopy.addAll(newSortLists);
         if (result) {
-            this.setSorts(sortsCopy.toArray(new SortDescriptor[sortsCopy.size()]));
+            this.setSorts(sortsCopy.toArray(new SortDescriptorBase[sortsCopy.size()]));
         }
-        return result;
+        return this;
     }
 
-    public boolean removeSort(SortDescriptorBase sort) {
+    public DynamicQuery<T> removeSort(SortDescriptorBase sort) {
         List<SortDescriptorBase> sortsCopy =
                 sorts == null ? new ArrayList<>() : new ArrayList<>(Arrays.asList(sorts));
         boolean result = sortsCopy.remove(sort);
         if (result) {
-            this.setSorts(sortsCopy.toArray(new SortDescriptor[sortsCopy.size()]));
+            this.setSorts(sortsCopy.toArray(new SortDescriptorBase[sortsCopy.size()]));
         }
-        return result;
+        return this;
+    }
+
+    public DynamicQuery<T> addFilterDescriptor(
+            FilterCondition condition, Function<T, Object> getFieldFunc,
+            FilterOperator operator, Object value) {
+        FilterDescriptor filterDescriptor = new FilterDescriptor(condition, this.entityClass, getFieldFunc, operator, value);
+        return addFilter(filterDescriptor);
+    }
+
+    public DynamicQuery<T> addFilterDescriptor(
+            Function<T, Object> getFieldFunc,
+            FilterOperator operator, Object value) {
+        FilterDescriptor filterDescriptor = new FilterDescriptor(FilterCondition.AND, this.entityClass, getFieldFunc, operator, value);
+        return addFilter(filterDescriptor);
+    }
+
+    public DynamicQuery<T> addSortDescriptor(Function<T, Object> getFieldFunc, SortDirection sortDirection) {
+        SortDescriptor sortDescriptor = new SortDescriptor(this.entityClass, getFieldFunc, sortDirection);
+        return addSorts(sortDescriptor);
     }
 }
--- src/test/java/com/github/wz2cool/dynamic/DynamicQueryTest.java
+++ src/test/java/com/github/wz2cool/dynamic/DynamicQueryTest.java
@@ -61,4 +61,38 @@ public class DynamicQueryTest {
         dynamicQuery.removeSort(sort);
         assertEquals(0, dynamicQuery.getSorts().length);
     }
+
+    @Test
+    public void testCreateQuery() {
+        DynamicQuery<Student> query = DynamicQuery.createQuery(Student.class);
+        assertEquals(true, query != null);
+    }
+
+    @Test
+    public void testAddFilterDescriptor() {
+        DynamicQuery<Student> query = DynamicQuery.createQuery(Student.class)
+                .addFilterDescriptor(FilterCondition.OR, Student::getName, FilterOperator.EQUAL, "frank");
+        FilterDescriptor filterDescriptor = (FilterDescriptor) query.getFilters()[0];
+        assertEquals(FilterCondition.OR, filterDescriptor.getCondition());
+        assertEquals("name", filterDescriptor.getPropertyPath());
+        assertEquals(FilterOperator.EQUAL, filterDescriptor.getOperator());
+        assertEquals("frank", filterDescriptor.getValue());
+    }
+
+    @Test
+    public void testLinkOperation() {
+        DynamicQuery<Student> query = DynamicQuery.createQuery(Student.class)
+                .addFilterDescriptor(Student::getName, FilterOperator.EQUAL, "frank")
+                .addSortDescriptor(Student::getAge, SortDirection.DESC);
+
+        FilterDescriptor filterDescriptor = (FilterDescriptor) query.getFilters()[0];
+        assertEquals(FilterCondition.AND, filterDescriptor.getCondition());
+        assertEquals("name", filterDescriptor.getPropertyPath());
+        assertEquals(FilterOperator.EQUAL, filterDescriptor.getOperator());
+        assertEquals("frank", filterDescriptor.getValue());
+
+        SortDescriptor sortDescriptor = (SortDescriptor) query.getSorts()[0];
+        assertEquals("age", sortDescriptor.getPropertyPath());
+        assertEquals(SortDirection.DESC, sortDescriptor.getSortDirection());
+    }
 }
