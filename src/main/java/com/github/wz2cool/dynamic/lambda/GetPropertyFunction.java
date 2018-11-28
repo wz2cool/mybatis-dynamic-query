c0fab0e70b4a1df8dd969743de8b5b82b51ba0b7
wz2cool@live.cn <wz2cool@live.cn>
2018-11-28 21:41:48 +0800

    remove jodd

--- src/main/java/com/github/wz2cool/dynamic/lambda/GetPropertyFunction.java
+++ src/main/java/com/github/wz2cool/dynamic/lambda/GetPropertyFunction.java
@@ -3,8 +3,6 @@ package com.github.wz2cool.dynamic.lambda;
 import java.io.Serializable;
 import java.util.function.Function;
 
-// https://zhuanlan.zhihu.com/p/37335673
 @FunctionalInterface
 public interface GetPropertyFunction<T> extends Function<T, Object>, Serializable {
-
 }
