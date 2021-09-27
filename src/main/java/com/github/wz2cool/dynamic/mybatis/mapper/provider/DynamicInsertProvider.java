04f1ae268be6ac27c588437bf372f80dc0a29156
jin.wang@innodealing.com <wajncn@gmail.com>
2021-09-27 10:16:15 +0800

    fix

--- src/main/java/com/github/wz2cool/dynamic/mybatis/mapper/provider/DynamicInsertProvider.java
+++ src/main/java/com/github/wz2cool/dynamic/mybatis/mapper/provider/DynamicInsertProvider.java
@@ -107,24 +107,6 @@ public class DynamicInsertProvider {
     }
 
 
-
-    @Deprecated
-    public String dynamicSQL(ProviderContext providerContext) {
-        ProviderTable providerTable = ProviderFactory.create(providerContext);
-        Class<?> entityClass = providerTable.getEntityClass();
-        StringBuilder sql = new StringBuilder();
-        sql.append("SELECT");
-        sql.append(String.format("<if test=\"%s.%s\">distinct</if>",
-                MapperConstants.DYNAMIC_QUERY_PARAMS, MapperConstants.DISTINCT));
-        //支持查询指定列
-        sql.append(DynamicQuerySqlHelper.getSelectColumnsClause());
-        sql.append("from " + providerTable.getTableName() + " ");
-        sql.append(DynamicQuerySqlHelper.getWhereClause(entityClass));
-        sql.append(DynamicQuerySqlHelper.getSortClause());
-        return sql.toString();
-    }
-
-
     public String selectCountByDynamicQuery(ProviderContext providerContext) {
         ProviderTable providerTable = ProviderFactory.create(providerContext);
         Class<?> entityClass = providerTable.getEntityClass();
