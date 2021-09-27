0a859c06972041cfb9d7a631793da1c83e72ab1b
jin.wang@innodealing.com <wajncn@gmail.com>
2021-09-27 12:43:37 +0800

    fix

--- src/main/java/com/github/wz2cool/dynamic/mybatis/mapper/helper/DynamicQuerySqlHelper.java
+++ src/main/java/com/github/wz2cool/dynamic/mybatis/mapper/helper/DynamicQuerySqlHelper.java
@@ -37,7 +37,7 @@ public class DynamicQuerySqlHelper {
     }
 
     public static String getSelectColumnsClause() {
-        return String.format(" ${%s.%s} ", MapperConstants.DYNAMIC_QUERY_PARAMS, MapperConstants.SELECT_COLUMNS_EXPRESSION);
+        return CommonsHelper.format(" ${%s.%s} ", MapperConstants.DYNAMIC_QUERY_PARAMS, MapperConstants.SELECT_COLUMNS_EXPRESSION);
     }
 
 
@@ -48,10 +48,10 @@ public class DynamicQuerySqlHelper {
      * @return 动态条件
      */
     public static String getWhereClause(Class<?> entityClass) {
-        final String newExpression = String.format("%s.%s", MapperConstants.DYNAMIC_QUERY_PARAMS, MapperConstants.WHERE_EXPRESSION);
+        final String newExpression = CommonsHelper.format("%s.%s", MapperConstants.DYNAMIC_QUERY_PARAMS, MapperConstants.WHERE_EXPRESSION);
         StringBuilder sql = new StringBuilder();
         sql.append("<where>");
-        sql.append(String.format("<if test=\"%s != null and %s != ''\">${%s}</if>",
+        sql.append(CommonsHelper.format("<if test=\"%s != null and %s != ''\">${%s}</if>",
                 newExpression, newExpression, newExpression));
         sql.append("</where>");
         return sql.toString();
@@ -59,29 +59,29 @@ public class DynamicQuerySqlHelper {
 
     public static String getSortClause() {
         String newExpression = String.format("%s.%s", MapperConstants.DYNAMIC_QUERY_PARAMS, MapperConstants.SORT_EXPRESSION);
-        return String.format("<if test=\"%s != null and %s != ''\">ORDER BY ${%s}</if>",
+        return CommonsHelper.format("<if test=\"%s != null and %s != ''\">ORDER BY ${%s}</if>",
                 newExpression, newExpression, newExpression);
     }
 
     public static String getSetClause() {
-        String newExpression = String.format("%s.%s", MapperConstants.DYNAMIC_QUERY_PARAMS, MapperConstants.SET_EXPRESSION);
-        return String.format("<if test=\"%s != null and %s != ''\">SET ${%s}</if>",
+        String newExpression = CommonsHelper.format("%s.%s", MapperConstants.DYNAMIC_QUERY_PARAMS, MapperConstants.SET_EXPRESSION);
+        return CommonsHelper.format("<if test=\"%s != null and %s != ''\">SET ${%s}</if>",
                 newExpression, newExpression, newExpression);
     }
 
     public static String getSelectMax() {
-        return String.format("SELECT MAX(${%s})", MapperConstants.COLUMN);
+        return CommonsHelper.format("SELECT MAX(${%s})", MapperConstants.COLUMN);
     }
 
     public static String getSelectMin() {
-        return String.format("SELECT MIN(${%s})", MapperConstants.COLUMN);
+        return CommonsHelper.format("SELECT MIN(${%s})", MapperConstants.COLUMN);
     }
 
     public static String getSelectSum() {
-        return String.format("SELECT SUM(${%s})", MapperConstants.COLUMN);
+        return CommonsHelper.format("SELECT SUM(${%s})", MapperConstants.COLUMN);
     }
 
     public static String getSelectAvg() {
-        return String.format("SELECT AVG(${%s})", MapperConstants.COLUMN);
+        return CommonsHelper.format("SELECT AVG(${%s})", MapperConstants.COLUMN);
     }
 }
