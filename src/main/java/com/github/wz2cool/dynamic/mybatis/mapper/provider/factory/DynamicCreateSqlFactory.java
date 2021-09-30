9d04caecab2095edf3c1f0d31394ce2f0c0c1a5d
wangjin <1253398319@qq.com>
2021-09-30 21:18:25 +0800

    fix [*Selective]结尾的方法会自动忽略null值和空值

--- src/main/java/com/github/wz2cool/dynamic/mybatis/mapper/provider/factory/DynamicCreateSqlFactory.java
+++ src/main/java/com/github/wz2cool/dynamic/mybatis/mapper/provider/factory/DynamicCreateSqlFactory.java
@@ -3,9 +3,10 @@ package com.github.wz2cool.dynamic.mybatis.mapper.provider.factory;
 import com.github.wz2cool.dynamic.helper.CommonsHelper;
 import com.github.wz2cool.dynamic.mybatis.mapper.constant.MapperConstants;
 import com.github.wz2cool.dynamic.mybatis.mapper.helper.DynamicQuerySqlHelper;
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
 
+import javax.annotation.Nullable;
 import java.util.Arrays;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -66,16 +67,22 @@ public final class DynamicCreateSqlFactory {
             sqlBuilder.append("insert into ");
             sqlBuilder.append(providerTable.getTableName());
             sqlBuilder.append("<trim prefix=\"(\" suffix=\")\" suffixOverrides=\",\">");
+
+            final String template = "and %s != ''";
             for (ProviderColumn column : providerTable.getColumns()) {
-                sqlBuilder.append(CommonsHelper.format("<if test=\"%s != null\">%s,</if>",
-                        column.getJavaColumn(), column.getDbColumn()));
+                final String stringIf = column.getColumnType().isAssignableFrom(String.class)
+                        ? CommonsHelper.format(template, column.getJavaColumn()) : "";
+                sqlBuilder.append(CommonsHelper.format("<if test=\"%s != null %s\">%s,</if>",
+                        column.getJavaColumn(), stringIf, column.getDbColumn()));
             }
             sqlBuilder.append("</trim>");
 
             sqlBuilder.append("<trim prefix=\"values (\" suffix=\")\" suffixOverrides=\",\">");
             for (ProviderColumn column : providerTable.getColumns()) {
-                sqlBuilder.append(CommonsHelper.format("<if test=\"%s != null\">#{%s},</if>",
-                        column.getJavaColumn(), column.getJavaColumn()));
+                final String stringIf = column.getColumnType().isAssignableFrom(String.class)
+                        ? CommonsHelper.format(template, column.getJavaColumn()) : "";
+                sqlBuilder.append(CommonsHelper.format("<if test=\"%s != null %s\">#{%s},</if>",
+                        column.getJavaColumn(),stringIf, column.getJavaColumn()));
             }
             sqlBuilder.append("</trim>");
             sqlBuilder.append("</script>");
@@ -106,32 +113,12 @@ public final class DynamicCreateSqlFactory {
         if (providerTable.getPrimaryKey() == null) {
             throw new IllegalArgumentException(CommonsHelper.format("该类[%s]没有发现主键", providerTable.getTableName()));
         }
-        if (updateSelective) {
-            StringBuilder sqlBuilder = new StringBuilder();
-            sqlBuilder.append("<script>");
-            sqlBuilder.append(CommonsHelper.format("update %s ", providerTable.getTableName()));
-            sqlBuilder.append("<set>");
-            sqlBuilder.append(Arrays.stream(providerTable.getColumns())
-                    .filter(a -> !a.isPrimaryKey())
-                    .map(a -> CommonsHelper.format("<if test=\"%s != null %s\">%s = #{%s},</if>",
-                            a.getJavaColumn(),
-                            Objects.equals(a.getColumnType().getSimpleName(),
-                                    "String") ? CommonsHelper.format("and %s != ''", a.getJavaColumn()) : "",
-                            a.getDbColumn(), a.getJavaColumn()))
-                    .collect(Collectors.joining()));
-            sqlBuilder.append("</set>");
-            sqlBuilder.append(CommonsHelper.format(" where %s = #{%s}",
-                    providerTable.getPrimaryKey().getDbColumn(), providerTable.getPrimaryKey().getJavaColumn()));
-            sqlBuilder.append("</script>");
-            return sqlBuilder.toString();
-        }
         StringBuilder sqlBuilder = new StringBuilder();
         sqlBuilder.append("<script>");
-        sqlBuilder.append(CommonsHelper.format("update %s set ", providerTable.getTableName()));
-        sqlBuilder.append(Arrays.stream(providerTable.getColumns())
-                .filter(a -> !a.isPrimaryKey())
-                .map(a -> CommonsHelper.format("%s = #{%s}", a.getDbColumn(), a.getJavaColumn()))
-                .collect(Collectors.joining(",")));
+        sqlBuilder.append(CommonsHelper.format("update %s ", providerTable.getTableName()));
+        sqlBuilder.append("<set>");
+        sqlBuilder.append(this.getDynamicDefaultIfSet(null, updateSelective, true));
+        sqlBuilder.append("</set>");
         sqlBuilder.append(CommonsHelper.format(" where %s = #{%s}",
                 providerTable.getPrimaryKey().getDbColumn(), providerTable.getPrimaryKey().getJavaColumn()));
         sqlBuilder.append("</script>");
@@ -180,6 +167,7 @@ public final class DynamicCreateSqlFactory {
         sqlBuilder.append("<script>");
         //add bind
         sqlBuilder.append(DynamicQuerySqlHelper.getBindFilterParams(true));
+        //count(*) 和count(0)  一样的
         sqlBuilder.append("select COUNT(*) from " + providerTable.getTableName() + " ");
         sqlBuilder.append(DynamicQuerySqlHelper.getWhereClause(entityClass));
         sqlBuilder.append("</script>");
@@ -197,6 +185,9 @@ public final class DynamicCreateSqlFactory {
         sqlBuilder.append(DynamicQuerySqlHelper.getUpdateBindFilterParams(true));
         sqlBuilder.append(CommonsHelper.format("update %s ", providerTable.getTableName()));
         sqlBuilder.append(DynamicQuerySqlHelper.getSetClause());
+
+//        sqlBuilder.append(this.getDynamicDefaultIfSet("entity", true));
+
         sqlBuilder.append(DynamicQuerySqlHelper.getWhereClause(entityClass));
         sqlBuilder.append("</script>");
         return sqlBuilder.toString();
@@ -209,19 +200,74 @@ public final class DynamicCreateSqlFactory {
         sqlBuilder.append(DynamicQuerySqlHelper.getBindFilterParams(true));
         sqlBuilder.append(CommonsHelper.format("update %s ", providerTable.getTableName()));
         sqlBuilder.append("<set>");
-        sqlBuilder.append(Arrays.stream(providerTable.getColumns())
-                .filter(a -> !a.isPrimaryKey())
-                // entity为别名  @see UpdateByDynamicQueryMapper#updateByDynamicQuery
-                .map(a -> CommonsHelper.format("<if test=\"entity.%s != null %s\">%s = #{entity.%s},</if>",
-                        a.getJavaColumn(),
-                        Objects.equals(a.getColumnType().getSimpleName(),
-                                "String") ? CommonsHelper.format("and entity.%s != ''", a.getJavaColumn()) : "",
-                        a.getDbColumn(), a.getJavaColumn()))
-                .collect(Collectors.joining()));
+        sqlBuilder.append(this.getDynamicDefaultIfSet("entity", true, true));
         sqlBuilder.append("</set>");
         sqlBuilder.append(DynamicQuerySqlHelper.getWhereClause(providerTable.getEntityClass()));
         sqlBuilder.append("</script>");
         System.out.println(sqlBuilder.toString());
         return sqlBuilder.toString();
+    }
+
+
+    //    end
+    //#####################################################################################################
+    //############################################公共私有方法,请勿更改######################################
+    //#####################################################################################################
+    private static final String if_set_template = "<if test=\"aliasName%s != null\">%s = #{aliasName%s},</if>";
+    private static final String if_set_string_template = "<if test=\"aliasName%s != null and aliasName%s != ''\">%s = #{aliasName%s},</if>";
+    private static final String set_template = "%s = #{aliasName%s}";
+    private static final String ALIAS_NAME = "aliasName";
+
+
+    /**
+     * @param aliasName       是否生成别名:
+     *                        假设传了aaa:  #{aaa.parseMethod}
+     *                        如果不传则为默认:  #{parseMethod}
+     * @param setIf           是否生成带if的:
+     *                        带if的: <if test="test.parseMethod != null and test.parseMethod != ''">parse_method = #{test.parseMethod},</if>
+     *                        不带if的: parse_method = #{aaaaa.parseMethod}
+     * @param breakPrimaryKey 是否跳过主键. 不生成主键的条件
+     * @return sql
+     */
+    private String getDynamicDefaultIfSet(@Nullable String aliasName, boolean setIf, boolean breakPrimaryKey) {
+        final String aliasNameNew = aliasName == null ? "" : aliasName + ".";
+        final StringBuilder sqlBuilder = new StringBuilder();
+
+        if (setIf) {
+            final String setIfTemplate = if_set_template.replace(ALIAS_NAME, aliasNameNew);
+            final String setIfStringTemplate = if_set_string_template.replace(ALIAS_NAME, aliasNameNew);
+            for (ProviderColumn column : providerTable.getColumns()) {
+                if (column.isPrimaryKey && breakPrimaryKey) {
+                    continue;
+                }
+                if (column.getColumnType().isAssignableFrom(String.class)) {
+                    sqlBuilder.append(CommonsHelper.format(setIfStringTemplate, column.getJavaColumn(), column.getJavaColumn()
+                            , column.getDbColumn(), column.getJavaColumn()));
+                } else {
+                    sqlBuilder.append(CommonsHelper.format(setIfTemplate, column.getJavaColumn(), column.getDbColumn(), column.getJavaColumn()));
+                }
+            }
+            return sqlBuilder.toString();
+
+        }
+
+
+        final String setTemplate = set_template.replace(ALIAS_NAME, aliasNameNew);
+        for (ProviderColumn column : providerTable.getColumns()) {
+            if (column.isPrimaryKey) {
+                continue;
+            }
+            sqlBuilder.append(CommonsHelper.format(setTemplate, column.getDbColumn(), column.getJavaColumn()));
+            sqlBuilder.append(",");
+        }
+        sqlBuilder.deleteCharAt(sqlBuilder.lastIndexOf(","));
+        return sqlBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        DynamicCreateSqlFactory sqlFactory = DynamicCreateSqlFactory.getSqlFactory(ProviderTableHelper.getProviderTable(BIConversion.User.class));
+        System.out.println(sqlFactory.getDynamicDefaultIfSet("test", true, false));
+        System.out.println(sqlFactory.getDynamicDefaultIfSet("test", true, true));
+
     }
 }
