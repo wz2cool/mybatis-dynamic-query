d5991ffbb0f181abbee57b46b9fc05f688f3677a
wz2cool <wz2cool@live.cn>
2022-08-14 17:19:14 +0800

    add create

--- src/main/java/com/github/wz2cool/dynamic/FilterGroupDescriptor.java
+++ src/main/java/com/github/wz2cool/dynamic/FilterGroupDescriptor.java
@@ -13,6 +13,24 @@ public class FilterGroupDescriptor<T>
 
     private FilterCondition condition = FilterCondition.AND;
 
+    private Class<T> clazz;
+
+    public FilterGroupDescriptor() {
+        // hide construct
+    }
+
+    public FilterGroupDescriptor(Class<T> clazz) {
+        this.clazz = clazz;
+    }
+
+    public static <T> FilterGroupDescriptor<T> create(Class<T> clazz) {
+        return new FilterGroupDescriptor<>(clazz);
+    }
+
+    public Class<T> getClazz() {
+        return clazz;
+    }
+
     @Override
     public FilterCondition getCondition() {
         return condition;
